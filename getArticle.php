b25a1b00ea1caaeb15d922f730f4dbf85b86b5d3
iklone <jamesthackway@hotmail.co.uk>
2020-11-26 01:52:22 +0000

    Added post page loading through rewrite engine. Added test post files.

---
+++ getArticle.php
@@ -0,0 +1,17 @@
+<?php
+    //connect to DB
+    include "./connect.php";
+
+    //get blog id
+    $id = $_GET['id'];
+
+    //build query
+    $query = "SELECT title, publishDate, img, body FROM posts WHERE id=" . $id . ";";
+
+    //execute query
+    $result = mysqli_query($conn, $query);
+    $row = mysqli_fetch_assoc($result);
+
+    //echo JS func to build blog data
+    echo "loadData(" . $row["title"] . ", " . $row["publishDate"] . ", " . $row["img"] . ", " . $row["body"] . ");";
+?>
