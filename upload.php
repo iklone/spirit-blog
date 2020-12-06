8a3cea62b0cdff2a665feada6b838ee055c532fe
iklone <jamesthackway@hotmail.co.uk>
2020-12-06 15:59:19 +0000

    Added upload post page, password file is hidden.

---
+++ upload.php
@@ -0,0 +1,30 @@
+<?php
+    //test password
+    $password = $_POST["password"];
+    include "./passwordCheck.php";
+    if ($password != $realPassword) {
+        die("Incorrect Password");
+    }
+
+    //get vars from post
+    $title = $_POST["title"];
+    $idText = $_POST["idText"];
+    $imgURL = $_POST["imgURL"];
+    $body = $_POST["body"];
+
+    //save body to file
+    $newFileURL = "./posts/" . $idText;
+    $newPostFile = fopen($newFileURL, "w") or die("Unable to create file");
+    fwrite($newPostFile, $body);
+    fclose($newPostFile);
+
+     //Save metadata to DB
+    //connect to DB
+    include "./connect.php";
+
+    //build query
+    $query = 'INSERT INTO `posts` (`title`, `img`, `body`, `idText`) VALUES ("' . $title . '", "' . $imgURL  . '", "' . $idText . '", "' . $idText . '");';
+
+    //execute query
+    $result = mysqli_query($conn, $query);
+?>
